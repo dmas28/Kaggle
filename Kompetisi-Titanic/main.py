a86d80f3dc010b2d57cb07f3b6a273d3745d5372
dmas28 <dimaspranowo1@gmail.com>
2022-06-03 23:52:49 +0700

    test commit

--- Kompetisi-Titanic/main.py
+++ Kompetisi-Titanic/main.py
@@ -1,2 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+dataset_train = pd.read_csv('~/Desktop/mywork/Kaggle/dataset/titanic/train.csv')
+dataset_test = pd.read_csv('~/Desktop/mywork/Kaggle/dataset/titanic/test.csv')
+contoh_submission = pd.read_csv('~/Desktop/mywork/Kaggle/dataset/titanic/gender_submission.csv')
+
+print(dataset_train)
